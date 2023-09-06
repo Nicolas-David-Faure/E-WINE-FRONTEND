c3c0291ed81c9b6d0c49187c134923c02d15e32c
marcos <marcossolis023@gmail.com>
2023-09-06 11:45:10 -0300

    create grid

--- src/commons/card/Card.jsx
+++ src/commons/card/Card.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import "./scss/card.scss";
 
 const Card = ({ producto }) => {
   return (
-    <div className="card__main">
-      <img src={producto.image} alt={producto.name} />
-      <h2>{producto.name}</h2>
-      <h3>{"$" + producto.price}</h3>
+    <div className="vino">
+      <img className="img" src={producto.image} alt={producto.name} />
+      <h2 className="h2">{producto.name}</h2>
+      <h3 className="precio">{"$" + producto.price}</h3>
       <p>{producto.description}</p>
+      <button className="boton-comprar">Comprar</button>
     </div>
   );
 };
